3118747fa063563757e867eb43c70c1bb09c74ba
relikd <info@relikd.de>
2018-12-11 11:58:53 +0100

    Fixed collection type NSSet -> NSArray

--- RSXML/RSParsedFeed.m
+++ RSXML/RSParsedFeed.m
@@ -10,7 +10,7 @@
 
 @implementation RSParsedFeed
 
-- (instancetype)initWithURLString:(NSString *)urlString title:(NSString *)title link:(NSString *)link articles:(NSSet *)articles {
+- (instancetype)initWithURLString:(NSString *)urlString title:(NSString *)title link:(NSString *)link articles:(NSArray *)articles {
 	
 	self = [super init];
 	if (!self) {
